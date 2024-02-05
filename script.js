39da596d3e5c3e21e0c22b6a764c5a3d4174e9c5
Andy Osborne <andyjamesosborne@proton.me>
2024-02-05 15:56:48 +0000

    feat: added more event listeners

--- script.js
+++ script.js
@@ -1,34 +1,25 @@
+const sunScreen = document.getElementById("sun-button")
+sunScreen.addEventListener("click", spaceInfoSun);
+
+const sunInfo = document.getElementById("sun-info");
+sunInfo.style.display = "none";
+
 const startScreen = document.querySelector(".start-screen");
-startScreen.addEventListener("click", spaceInfo);
 
-const sunScreen = document.querySelector(".sunny");
-sunScreen.style.display = "none";
-
-const mercuryScreen = document.querySelector(".first");
-mercuryScreen.style.display = "none";
-
-const venusScreen = document.querySelector(".second");
-venusScreen.style.display = "none";
-
-const earthScreen = document.querySelector(".third");
-earthScreen.style.display = "none";
-
-const marsScreen = document.querySelector(".fourth");
-marsScreen.style.display = "none";
-
-const jupiterScreen = document.querySelector(".fourth");
-jupiterScreen.style.display = "none";
-
-
-function spaceInfo() {
-    startScreen.style.display = "none";  // Hide start screen
-    sunScreen.style.display = "block";  // Show game screen
+function spaceInfoSun() {  
+  startScreen.style.display = "none";
+  sunInfo.style.display = "block";
+  returnSpace.style.display = "block";
 }
 
-  const returnSpaceButton = document.querySelector(".return-space");
-  returnSpaceButton.addEventListener("click", returnSpace)
+const returnSpace = document.getElementById("return-space")
+returnSpace.style.display = "none";
 
-function returnSpace() {
-  startScreen.stlye.display ="block";
-  sunScreen.style.display = "none";
+const returnButton = document.getElementById("return-space-button");
+returnSpace.addEventListener("click", returnHomeScreen);
+
+function returnHomeScreen() {
+  startScreen.style.display = "block";
+  sunInfo.style.display = "none";
+  returnSpace.style.display = "none";
 }
